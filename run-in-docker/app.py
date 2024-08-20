7b152f512716f42061d2227f94d5f134f30cd684
Rahees9983 <38326225+Rahees9983@users.noreply.github.com>
2024-08-20 10:09:23 +0530

    Update app.py

--- run-in-docker/app.py
+++ run-in-docker/app.py
@@ -73,4 +73,4 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="8080")
+    app.run(debug=True, host="0.0.0.0", port="8080")
